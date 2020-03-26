e26e31757d8648b335a65de873768ff370997724
LEMARK-J-MAC <lemark-j-mac@LEMARK-J-MAC.local>
2020-03-26 00:00:38 -0400

    adding files to github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./card-list/card-list.component";
+import { SearchBox } from "./search-box/search-box.component";
+
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cats: [],
+      searchField: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ cats: users }));
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { cats, searchField } = this.state;
+    const filteredCats = cats.filter(cats =>
+      cats.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Cats Rolodex</h1>
+        <SearchBox placeholder="search cats" handleChange={this.handleChange} />
+        <CardList cats={filteredCats} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/card/card.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./card.styles.css"
+
+export const Card = (props) => {
+    return (
+        <div className="card-container">
+            <img src={`https://robohash.org/${props.cat.id}?set=set4&size=180x180`} alt="cat" />
+            <h2>{props.cat.name}</h2>
+            <p>{props.cat.email}</p>
+        </div>
+    );
+}
